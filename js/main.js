fdd0c405bec493e95235705cf3c6683b0d965f21
RJacome <jacome@dccolorweb.com>
2016-04-08 00:10:30 -0600

    fixed menu on mobile, fixed stiky

--- js/main.js
+++ js/main.js
@@ -1,56 +1,33 @@
 $(document).ready(function(){
+    
+    
+
     $(".Jnav_btn").click(function(){
         Jnav();
     });
 });
 
 $(window).resize(function(){
-    var mediaquery = window.matchMedia("(min-width: 900px)");
+    var mediaquery = window.matchMedia("(min-width: 990px)");
         if (mediaquery.matches) {
             $(".Jnav").show().removeClass("active");
+            $("#main-nav").removeClass("active").show();
+            $(".btns").show();
+        }else{
+            $.SmartMenus.hideAll();
+            $("#main-nav").removeClass("active").slideUp(300);
+            $(".btns").slideUp(300);
         }
 });
-
 function Jnav(){
-    if($(".Jnav").hasClass("active")){
-        $(".Jnav").removeClass("active").slideUp(300);
+    if($("#main-nav").hasClass("active")){
+        $("#main-nav").removeClass("active").slideUp(300);
+        $(".btns").slideUp(300);
+        $.SmartMenus.hideAll();
     }else{
-        $(".Jnav").addClass("active").slideDown(300);
+       $("#main-nav").addClass("active").slideDown(300);
+       $(".btns").slideDown(300)
     }
 }
 
 
-/*function sticky_relocate() {
-    var window_top = $(window).scrollTop();
-    var div_top = $('#sticky-anchor').offset().top;
-    if (window_top > div_top) {
-        $('#sticky').addClass('stick');
-        $('#sticky-anchor').height($('#sticky').outerHeight());
-    } else {
-        $('#sticky').removeClass('stick');
-        $('#sticky-anchor').height(0);
-    }
-}
-
-$(function() {
-    $(window).scroll(sticky_relocate);
-    sticky_relocate();
-});
-
-var dir = 1;
-var MIN_TOP = 200;
-var MAX_TOP = 350;
-
-function autoscroll() {
-    var window_top = $(window).scrollTop() + dir;
-    if (window_top >= MAX_TOP) {
-        window_top = MAX_TOP;
-        dir = -1;
-    } else if (window_top <= MIN_TOP) {
-        window_top = MIN_TOP;
-        dir = 1;
-    }
-    $(window).scrollTop(window_top);
-    window.setTimeout(autoscroll, 100);
-}
-*/
