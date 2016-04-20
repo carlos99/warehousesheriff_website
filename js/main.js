ea2d409240ca4ebc176a30262310764861eadfc6
RJacome <jacome@dccolorweb.com>
2016-04-20 07:14:37 -0600

    add menu funcionality

--- js/main.js
+++ js/main.js
@@ -1,68 +1,16 @@
 $(document).ready(function(){
-    $(".Jnav_btn").click(function(){
-        Jnav();
+
+    $("button.navbar-toggler").click(function(){
+        collapseAll();
+    });
+
+$('a.navbar-toggler').on('click', function(event) {
+ 
+        $(this).parent().siblings().removeClass('open').find("ul").collapse('hide');
     });
 });
 
-$(window).resize(function(){
-    var mediaquery = window.matchMedia("(min-width: 900px)");
-        if (mediaquery.matches) {
-            $(".Jnav").show().removeClass("active");
-        }
-});
 
-function Jnav(){
-    if($(".Jnav").hasClass("active")){
-        $(".Jnav").removeClass("active").slideUp(300);
-    }else{
-        $(".Jnav").addClass("active").slideDown(300);
-    }
+function collapseAll(){
+    $('a.navbar-toggler').parent().find('ul').removeClass("in");
 }
-
-
-(function($){
-	$(document).ready(function(){
-	$('ul.dropdown-menu [data-toggle=dropdown]').on('click', function(event) {
-		event.preventDefault(); 
-		event.stopPropagation(); 
-			$(this).parent().siblings().removeClass('open');
-			$(this).parent().toggleClass('open');
-		});
-	});
-})(jQuery);
-
-
-/*function sticky_relocate() {
-    var window_top = $(window).scrollTop();
-    var div_top = $('#sticky-anchor').offset().top;
-    if (window_top > div_top) {
-        $('#sticky').addClass('stick');
-        $('#sticky-anchor').height($('#sticky').outerHeight());
-    } else {
-        $('#sticky').removeClass('stick');
-        $('#sticky-anchor').height(0);
-    }
-}
-
-$(function() {
-    $(window).scroll(sticky_relocate);
-    sticky_relocate();
-});
-
-var dir = 1;
-var MIN_TOP = 200;
-var MAX_TOP = 350;
-
-function autoscroll() {
-    var window_top = $(window).scrollTop() + dir;
-    if (window_top >= MAX_TOP) {
-        window_top = MAX_TOP;
-        dir = -1;
-    } else if (window_top <= MIN_TOP) {
-        window_top = MIN_TOP;
-        dir = 1;
-    }
-    $(window).scrollTop(window_top);
-    window.setTimeout(autoscroll, 100);
-}
-*/
