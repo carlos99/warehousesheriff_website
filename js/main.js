07b234cac4310c593d79dadbb42d289d022e3c2b
Adiel Hercules <jadher.11x2@gmail.com>
2016-05-15 20:41:55 -0600

    personal.html changes and global rules

--- js/main.js
+++ js/main.js
@@ -7,6 +7,28 @@ $(document).ready(function(){
 $('a.navbar-toggler').on('click', function(event) {
         $(this).parent().siblings().removeClass('open').find("ul").collapse('hide');
     });
+
+
+	$('body').on('click', '.btn-show-searchbox', function(e) {
+		var $parent = $(this).closest('.input-group');
+		$parent.addClass('is-focused');
+		$parent.find('.form-control').trigger('focus');
+	}).on('blur', '.search-add-on .form-control', function(e) {
+		var $parent = $(this).closest('.input-group');
+
+		if(window.hideSearchTimer != undefined) {
+			clearTimeout(window.hideSearchTimer);
+		}
+
+		window.hideSearchTimer = setTimeout(function() {
+			$parent.removeClass('is-focused');
+		}, 1000);
+	}).on('focus', '.search-add-on .form-control', function(e) {
+		if(window.hideSearchTimer != undefined) {
+			clearTimeout(window.hideSearchTimer);
+		}
+	});
+
 });
 
 
